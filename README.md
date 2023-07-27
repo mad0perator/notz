f7cd177421981f81bd73c280433afab836f3a804
mad0perator <mad0perator.creationlounge@gmail.com>
2023-07-27 09:28:09 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# NoTZ Discord Tag Creator
+
+Create a tag for use with Discord to display the correct time for all
+users regardless of their timezones.
+
+```sh
+notz Tomorrow at 9 PM
+```
---
+++ notz.fish
@@ -0,0 +1,15 @@
+# ~/fish/.config/functions/notz.fish
+#
+# NoTZ --- No Timezone Tag Creator --- a script for use with Discord.
+#
+# Create a dynamic date/time tag for use with Discord chat.
+# This eliminates the problem across multiple timezones.
+# ***NOTE*** Time will be local but tag will be UTC time since last epoch
+# Just copy and paste the tag into the chat!
+#
+function notz \
+    --wraps=date \
+    --description="Convert date/time or current time to dynamic Discord tag. (no TZ)"
+    echo "<t:$(date --date "$argv" +%s)>"
+end
+    
